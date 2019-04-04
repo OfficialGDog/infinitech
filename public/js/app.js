3e3c9bd788f7a962ac830a4d484cbf83ad729f87
Gareth Davies <garethdavieslive@gmail.com>
2019-04-04 15:48:20 +0100

    Filter by Category / Project name and tidied up some code...

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,93 @@
 const colors = ['green', 'purple', 'orange', 'red'];
 var showAdminMessage = false;
+var expenses = null;
+var filteredCategories = null;
+var filteredProject = null;
 
 var app = new Framework7({
+  picker: {
+    toolbarCloseText: ""
+  },
 on: {
   init: function () {
   console.log('App initialized');
 },
 pageInit: function (page) {
 if(page.name=='expenses'){
-  if(selected_index != null && filteredCategories != null && categories != null){
-    let filteredExpenses = categories.filter(item => item.Category === filteredCategories[selected_index].name);
-    for(var i = 0; i < filteredExpenses.length; i++){
-      let randomcolor = colors[Math.floor(Math.random() * colors.length)];
-      document.getElementById("expenses").innerHTML += "<div class='card theme-" + randomcolor + "' style='margin-top: 50px'><div class='card__part card__part-2 card__hover'><div class='card__part__side m--back'><div class='card__part__inner card__face' style='box-shadow: 5px 10px 8px #888888; height: 100px;'><div class='card__face__colored-side'></div><div style='float: right;position: relative;top: 0px;padding-right: 10px;' class='card__greytext'><span>" + filteredExpenses[i].Amount + "</span><i class='f7-icons' style='position: absolute;top: 25px;font-size: 20px;padding-left: 5px'>chevron_right</i></div><h3 class='card__face__price ng-binding text-limited'>" + filteredExpenses[i].Client_Project + "</h3><h4 class='card__greytext' style='font-weight: normal;margin: auto;margin-top: 15px;margin-bottom: 15px;'>" + filteredExpenses[i].Client_Name + ", " + filteredExpenses[i].Category + "</h4><p class='card__greytext' style='width: 80%;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;'>" + filteredExpenses[i].Expense_Desc + "</p><div class='card__greytext' style='font-size: 15px'><i class='f7-icons' style='font-size: 18px; padding-right: 10px'>time</i>Awaiting confirmation...</div></div></div></div></div>";
+  if(selected_index != null && filteredCategories != null && filteredProject != null && expenses != null){
+    let filteredExpenses = null;
+    let type = null;
+    let value = document.getElementById("filter").value;
+    switch(value)
+    {
+      case "Categories":
+      filteredExpenses = expenses.filter(item => item.Category === filteredCategories[selected_index].name);
+      type = "category";
+      break;
+      case "Project Name":
+      filteredExpenses = expenses.filter(item => item.Client_Project === filteredProject[selected_index].name);
+      type = "projectname";
+      break;
+      default:
+      console.error("Unable to filter results, no definition for: " + value);
+      break;
+    }
+    
+    if(filteredExpenses != null){
+      for(var i = 0; i < filteredExpenses.length; i++){
+        let title = null;
+        let subtitle = null;
+        switch(type){
+          case "category":
+            title = filteredExpenses[i].Category;
+            subtitle = filteredExpenses[i].Client_Project;
+          break;
+          case "projectname":
+            title = filteredExpenses[i].Client_Project;
+            subtitle = filteredExpenses[i].Category;
+          break;
+        }
+
+        let randomcolor = colors[Math.floor(Math.random() * colors.length)];
+        document.getElementById("expenses").innerHTML += "<div class='card theme-" + randomcolor + "' style='margin-top: 50px'><div class='card__part card__part-2 card__hover'><div class='card__part__side m--back'><div class='card__part__inner card__face' style='box-shadow: 5px 10px 8px #888888; height: 100px;'><div class='card__face__colored-side'></div><div style='float: right;position: relative;top: 0px;padding-right: 10px;' class='card__greytext'><span>" + filteredExpenses[i].Amount + "</span><i class='f7-icons' style='position: absolute;top: 25px;font-size: 20px;padding-left: 5px'>chevron_right</i></div><h3 class='card__face__price ng-binding text-limited'>" + title + "</h3><h4 class='card__greytext' style='font-weight: normal;margin: auto;margin-top: 15px;margin-bottom: 15px;'>" + filteredExpenses[i].Client_Name + ", " + subtitle + "</h4><p class='card__greytext' style='width: 80%;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;'>" + filteredExpenses[i].Expense_Desc + "</p><div class='card__greytext' style='font-size: 15px'><i class='f7-icons' style='font-size: 18px; padding-right: 10px'>time</i>Awaiting confirmation...</div></div></div></div></div>";
+      }
     }
     selected_index = null;
   }
 }
-else if(page.name=='categories' && showAdminMessage){
+else if(page.name=='categories'){
+  var filterPicker = app.picker.create({
+    inputEl: '#filter',
+    cols: [
+      {
+        textAlign: 'center',
+        values: ['Categories', 'Project Name']
+      }
+    ],
+    on: {
+    change: function (picker, values, displayValues) {
+      if(document.getElementById("filter").value != displayValues){
+        const column = picker.cols[0];
+        switch(displayValues[0]){
+          case column.values[0]:
+          // First column selected run code here
+          console.log("Filtering Expenses By Category...")
+          displayCategories(filteredCategories);
+          break;
+          case column.values[1]:
+          // Second column selected run code here
+          console.log("Filtering Expenses By Project...");
+          displayCategories(filteredProject);
+          break;
+        }
+          filterPicker.close();
+      }
+    }
+  },
+});
+if(showAdminMessage){
   document.getElementById("categories").innerHTML = "<h1>Admin has logged in.</h1>";
+}
 }
 },
 },
@@ -39,8 +108,6 @@ else if(page.name=='categories' && showAdminMessage){
 
 var mainView = app.views.create('.view-main');
 
-var categories = null;
-var filteredCategories = null;
 var selected_index = null;
 
 function loginOnKeyEnter(input1, input2, button){
@@ -58,16 +125,16 @@ function loginOnKeyEnter(input1, input2, button){
   });
 }
 
-function fetchCategories(id){
+function fetchExpenses(id){
   fetch("https://stormy-coast-58891.herokuapp.com/expenses/" + id)
   .then(response => response.json())
-  .then(response => {categories = response.slice(0); let array = response.map((item) => item.Category); let listofcategories = array.reduce(function(prev, cur) {prev[cur] = (prev[cur] || 0) + 1; return prev;}, {}); var newarray = Object.keys(listofcategories).map(item => ({name: item, count: listofcategories[item]})); filteredCategories = newarray.slice(0); displayCategories(response, newarray)})
+  .then(response => {expenses = response.slice(0); let array = response.map((item) => item.Category); let listofcategories = array.reduce(function(prev, cur) {prev[cur] = (prev[cur] || 0) + 1; return prev;}, {}); var categoryarray = Object.keys(listofcategories).map(item => ({name: item, count: listofcategories[item]})); filteredCategories = categoryarray.slice(0); displayCategories(categoryarray); let array2 =  response.map((item) => item.Client_Project); let listofprojects = array2.reduce(function(prev, cur) {prev[cur] = (prev[cur] || 0) + 1; return prev;}, {}); var projectarray = Object.keys(listofprojects).map(item => ({name: item, count: listofprojects[item]})); filteredProject = projectarray.slice(0);})
   .catch(err => console.error(err))
 }
 
-
-function displayCategories(allexpenses,categories){
+function displayCategories(categories){
   while(document.getElementById("categories").innerHTML != null){
+    document.getElementById("categories").innerHTML = "";
     if(categories.length > 0){
       for(var i = 0; i < categories.length; i++){
         let randomcolor = colors[Math.floor(Math.random() * colors.length)];
