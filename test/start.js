867d3f9a0e0edc71210b99c856de33eb194d40be
PeterDawson80 <peterdawson80@hotmail.com>
2019-05-07 19:18:12 +0100

    created 2 mock tests to ensure testing works as expected

---
+++ test/start.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+it('Should pass this mock test', function() {
+    const pass = true;
+    expect(pass).to.equal(true);
+})
+
+it('Should fail this mock test', function() {
+    const pass = false;
+    expect(pass).to.equal(true);
+})
